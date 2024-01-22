12b0faa8ae40034ede97bfa9b95ad02bd64865d5
Bubulle <bubulle86@yahoo.com>
2024-01-22 22:07:12 +0100

    More render

--- render.py
+++ render.py
@@ -2,6 +2,7 @@ import easygraphics as g
 import scenes
 
 FPS = 60
+COLORS = g.Color
 
 def init(width, height, main_loop, window_size):
     g.init_graph(width, height)
@@ -33,9 +34,33 @@ def saveFrame(file_name):
 def is_runing():
     return g.is_run()
 
+
 class Render:
-    def __init__(self, width, height):
+    def __init__(self, width, height, view_port=(0, 0, 2)):
         self.width = width
         self.height = height
+        self.center_x, self.center_y, self.real_width = view_port
+        self.real_height = self.height * self.real_width / self.width
+
+        self.offset_x = self.real_width / 2
+        self.offset_y = self.real_height / 2
+
+        self.factor_x = self.width / self.real_width
+        self.factor_y = self.height / self.real_height
+
+    def _get_virtual_x(self, x):
+        return (x + self.offset_x) * self.factor_x
+
+    def _get_virtual_y(self, y):
+        return (y + self.offset_y) * self.factor_y
+
+    def run(self, main_loop):
+        g.init_graph(self.width, self.height)
+        g.set_render_mode(g.RenderMode.RENDER_MANUAL)
+        main_loop()
+        g.close_graph()
+
+    def render_circle(self, x, y, size, border_color=COLORS.BLACK,
+                      background_color=COLORS.WHITE)
 
 
